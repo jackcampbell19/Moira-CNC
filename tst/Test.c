41d40e512f0b1549ef0bb0067060b091f7f41fe2
Jack Campbell <jackcampbell19@gmail.com>
2021-02-24 16:51:58 -0800

    Added additional tests and introduced c tests

---
+++ tst/Test.c
@@ -0,0 +1,3 @@
+#include "../src/hardware/CNC.h"
+
+void init_test_cnc(CNC* cnc, )
---
+++ tst/machine/mi/MachineInstructionTest.c
@@ -0,0 +1,6 @@
+#include "../../../src/"
+
+void test_run_good_mi_file() {
+    char[] path = "../../files/mi_good_coordinates_only.mi";
+    
+}
